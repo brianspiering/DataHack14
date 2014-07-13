e33d6d79251de18d0967239951b33caaf70cdd42
Vivek <3vivekb@gmail.com>
2014-07-12 21:36:05 -0700

    Revert 5ec8885..235ba4f
    This rolls back to commit 5ec888509a65ea584096d4acdcbe098530011490.

--- transfer_file_to_ec2.sh
+++ transfer_file_to_ec2.sh
@@ -1,3 +1,6 @@
-#!/usr/bin/env bash
-scp -i ~/.ssh/datahack14.pem ~/Documents/DataHack14/Saturday_Data/Mobility_Signaling_Peering_Traffic ubuntu@ec2-54-201-161-124.us-west-2.compute.amazonaws.com:~/.
-scp -i ~/.ssh/datahack14.pem temp.txt ubuntu@ec2-54-201-161-124.us-west-2.compute.amazonaws.com:
+scp -i ~/.ssh/datahack14_oregon.pem ~/Documents/DataHack14/Saturday_Data/Mobility_Signaling_Peering_Traffic.tar ubuntu@ec2-54-187-200-110.us-west-2.compute.amazonaws.com:/home/rstudio
+
+mv Mobility_Signaling_Peering_Traffic.tar /home/rstudio
+
+# untar
+
