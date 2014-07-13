dfd7d9c6732d7dcfdf99e25b4a6003b7a28f664a
Brian Spiering <bspiering@gmail.com>
2014-07-12 17:29:00 -0700

    Fix data writing bug

--- MobilityAnalyses/load_sample_of_large_data.r
+++ MobilityAnalyses/load_sample_of_large_data.r
@@ -1,3 +1,8 @@
+# General setup ----------------------------------------------------------------
+cat("\014")    # Clear console
+rm(list=ls())  # Delete all variables
+graphics.off() # Close all open plots
+
 base_path <- "~/Documents/DataHack14/"
 folder <- "Saturday_Data/" # "Tuesday_Data/" # 
 #file_name <- "Mobility_Signaling_Operator_Traffic"
@@ -7,19 +12,21 @@ end_point_in <- paste(base_path, folder, file_name, suffix, sep="")
 end_point_out <- paste(base_path, folder, file_name, "_subsample", suffix, sep="")
 
 cat(file_name, "\n")
-file_in <- file(end_point_in,"r")
-file_out <- file(end_point_out,"a")
+file_in <- file(end_point_in, "r")
+file_out <- file(end_point_out, "w")
 
 # Headers ------------------------------------------------------------------
 headers <- readLines(file_in, n=1)
-cat(headers, "\n")
+cat("Headers:", headers, "\n")
 writeLines(headers, file_out) # copy headers
 
 # Data ------------------------------------------------------------------
-sample_size <- 1000000
+sample_size <- 100000
 datasample <- readLines(file_in, n=sample_size)
 writeLines(datasample, file_out)
 
 # Wrap up ---------------------------------------------------------------------
 close(file_in)
 close(file_out)
+
+cat('\nSubsample data written.')
--- MobilityAnalyses/peering_traffic_production.R
+++ MobilityAnalyses/peering_traffic_production.R
@@ -20,21 +20,17 @@ end_point_in <- paste(base_path, folder, file_name, suffix, sep="")
 Mobility_Signaling_Peering_Traffic <- read.csv(end_point_in)
 
 # str(Mobility_Signaling_Peering_Traffic) # Look at data
+cat("Number of rows:", nrow(Mobility_Signaling_Peering_Traffic))
 
-# Make data frame numeric --------------------
+# Convert dataframe numeric --------------------
 Mobility_Signaling_Peering_Traffic$OUTBOUND_MESSAGES <- as.numeric(Mobility_Signaling_Peering_Traffic$OUTBOUND_MESSAGES)
 Mobility_Signaling_Peering_Traffic$INBOUND_MESSAGES <- as.numeric(Mobility_Signaling_Peering_Traffic$INBOUND_MESSAGES)
 
-
-# Primary Function-----------
+# Define aggregate function-----------
 outputAggs <- function(orig_country, dest_country) {
   both_country <- subset(Mobility_Signaling_Peering_Traffic, 
                          Mobility_Signaling_Peering_Traffic$DESTINATION_COUNTRY == dest_country 
                          & Mobility_Signaling_Peering_Traffic$ORIGINATION_COUNTRY == orig_country)
-  
-  #str(both_country)
-  #length(unique(both_country$PEERING_CUSTOMER_ID))
-  
   
   agg_outbound <- aggregate(both_country$OUTBOUND_MESSAGES ~ both_country$PEERING_CUSTOMER_ID,
                             data = both_country,
@@ -47,11 +43,10 @@ outputAggs <- function(orig_country, dest_country) {
   agg_outbound <-  agg_outbound[order(agg_outbound[,2], decreasing=TRUE),]
   #agg_outbound[1:10,]
   
-  
   agg_inbound <- agg_inbound[order(agg_inbound[,2], decreasing=TRUE),]
   #agg_inbound[1:10,]
   
-  # Load R object ----------------------------------------------------------------
+  # Save R object ----------------------------------------------------------------
   path <- "./data/"
   name <- paste(orig_country, dest_country)  
   type <- ".rdata"
@@ -64,4 +59,4 @@ outputAggs <- function(orig_country, dest_country) {
   
 }
 
-cat('Output saved.\n')
+outputAggs(orig_country, dest_country)
