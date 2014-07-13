939a08755609fcffbade3c99a86bdb2a4f0f15e0
Vivek <3vivekb@gmail.com>
2014-07-13 06:24:15 -0700

    Jsons created
    Ran it on my laptop - these sample jsons i believe are the complete
    ones!

--- transfer_file_to_ec2.sh
+++ transfer_file_to_ec2.sh
@@ -2,6 +2,7 @@ scp -i ~/.ssh/datahack14_oregon.pem ~/Documents/DataHack14/Saturday_Data/Mobilit
 
 sudo mv Mobility_Signaling_Peering_Traffic.tar /home/rstudio
 
+ssh -i ~/.ssh/datahack14_oregon.pem ubuntu@ec2-54-187-200-110.us-west-2.compute.amazonaws.com
 
 
 # untar
