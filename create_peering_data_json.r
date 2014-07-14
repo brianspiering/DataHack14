269f6f8003cd01a79704a09febdd65f7916be1ee
Brian Spiering <bspiering@gmail.com>
2014-07-14 10:31:12 -0700

    Clean up code

--- create_peering_data_json.r
+++ create_peering_data_json.r
@@ -1,3 +1,9 @@
+#!/usr/bin/env Rscript
+
+# File description -------------------------------------------------------------
+# Find aggreatate statisics by country
+#
+
 # List of packages for session
 .packages = c("rjson") 
 
@@ -16,15 +22,16 @@ graphics.off() # Close all open plots
 # Load data -----------
 cat('Loading data...\n')
 
-# Local box vs AWS
 if (version$os == "darwin10.8.0") {
-  # Local
+  # Local - mac
   base_path <- "~/Documents/DataHack14/"
   folder <- "Saturday_Data/"
   file_name <- "Mobility_Signaling_Peering_Traffic_subsample"
+} else if (version$os == "windows"){
+  
 } else if (version$os == "linux-gnu"){
-  # Remote 
-  dir.create("data") # make output dir
+  # Remote - aws
+  dir.create("output") # make output dir
   base_path <- ""
   folder <- ""
   file_name <- "Mobility_Signaling_Peering_Traffic_subsample"
@@ -33,21 +40,21 @@ if (version$os == "darwin10.8.0") {
 
 suffix <- ".csv"
 end_point_in <- paste(base_path, folder, file_name, suffix, sep="")
-Mobility_Signaling_Peering_Traffic <- read.csv(end_point_in)
+data_mobility_peering <- read.csv(end_point_in)
 
 # str(Mobility_Signaling_Peering_Traffic) # Look at data
-cat("Number of rows:", nrow(Mobility_Signaling_Peering_Traffic), "\n")
+cat("Number of rows:", nrow(data_mobility_peering), "\n")
 
 # Convert dataframe numeric --------------------
-Mobility_Signaling_Peering_Traffic$OUTBOUND_MESSAGES <- as.numeric(Mobility_Signaling_Peering_Traffic$OUTBOUND_MESSAGES)
-Mobility_Signaling_Peering_Traffic$INBOUND_MESSAGES <- as.numeric(Mobility_Signaling_Peering_Traffic$INBOUND_MESSAGES)
+data_mobility_peering$OUTBOUND_MESSAGES <- as.numeric(data_mobility_peering$OUTBOUND_MESSAGES)
+data_mobility_peering$INBOUND_MESSAGES <- as.numeric(data_mobility_peering$INBOUND_MESSAGES)
 
 # Define aggregate function-----------
 outputAggs <- function(company, orig_country, dest_country, file_name) {
   cat("Calculating sums & counts (aka doin' data science) \n")
-  both_country <- subset(Mobility_Signaling_Peering_Traffic, 
-                         Mobility_Signaling_Peering_Traffic$DESTINATION_COUNTRY == dest_country 
-                         & Mobility_Signaling_Peering_Traffic$ORIGINATION_COUNTRY == orig_country)
+  both_country <- subset(data_mobility_peering, 
+                         data_mobility_peering$DESTINATION_COUNTRY == dest_country 
+                         & data_mobility_peering$ORIGINATION_COUNTRY == orig_country)
   
   agg_outbound <- aggregate(both_country$OUTBOUND_MESSAGES ~ both_country$PEERING_CUSTOMER_ID,
                             data = both_country,
@@ -58,20 +65,15 @@ outputAggs <- function(company, orig_country, dest_country, file_name) {
                            FUN=sum)
   
   agg_outbound <-  agg_outbound[order(agg_outbound[,2], decreasing=TRUE),]
-  #agg_outbound[1:10,]
-  
   agg_inbound <- agg_inbound[order(agg_inbound[,2], decreasing=TRUE),]
-  print(agg_inbound)
-  #agg_inbound[1:10,]
   
   names(agg_outbound) <- c("customer_id", "outbound_messages")
   names(agg_inbound)<- c("customer_id", "inbound_messages")
   
-  agg_both <-merge(agg_outbound, agg_inbound, by= "customer_id")
-  # Save data to json -----------------------------------------------------------
-  
+  agg_both <- merge(agg_outbound, agg_inbound, by= "customer_id")
   agg_both$total_messages <- agg_both$inbound_messages + agg_both$outbound_messages
   total_data <- sum(agg_both$total_messages)
+  print(total_data)
   
   # Define new variables
   json <- numeric()
@@ -79,7 +81,8 @@ outputAggs <- function(company, orig_country, dest_country, file_name) {
   
   # Write out data for each customer / company team member
   for(i in 1:nrow(agg_outbound)){    
-    element <- list("cust_id" = as.numeric(agg_outbound[,1][i]), "data_used" = agg_outbound[,2][i])
+    element <- list("cust_id" = as.numeric(agg_outbound[,1][i]), 
+                    "data_used" = agg_outbound[,2][i])
     data_used <- append(data_used, list(element))  
   }
   
@@ -90,7 +93,7 @@ outputAggs <- function(company, orig_country, dest_country, file_name) {
                "total_data" = total_data, "team_members"=data_used)  
   
   # Define output file path  
-  path = "./data/"
+  path = "./output/"
   suffix = ".json"
   end_point_out <- paste(path, file_name, suffix, sep="")
   
@@ -101,16 +104,17 @@ outputAggs <- function(company, orig_country, dest_country, file_name) {
   cat("JSON filed saved.") 
 }
 
-# 1st json ------------------
+# Create json files ------------------------------------------------------------
+# 1st json
 company = "WPP"
 orig_country <- "U. K."
 dest_country <- "BRAZIL"
-file_name <- "sample_data_001"
+file_name <- "sample_output_001"
 outputAggs(company, orig_country, dest_country, file_name)
 
-# 2nd json ------------------
+# 2nd json
 company = "Facebook"
 orig_country<- "UNITED STATES"
 dest_country <- "BRAZIL"
-file_name <- "sample_data_002"
+file_name <- "sample_output_002"
 outputAggs(company, orig_country, dest_country, file_name)
---
+++ sub_sample_data.r
@@ -0,0 +1,35 @@
+# General setup ----------------------------------------------------------------
+cat("\014")    # Clear console
+rm(list=ls())  # Delete all variables
+graphics.off() # Close all open plots
+
+# Define variables
+sample_size <- 1000000
+
+# Define path
+base_path <- "~/Documents/DataHack14/"
+folder <- "Saturday_Data/" # "Tuesday_Data/" # 
+#file_name <- "Mobility_Signaling_Operator_Traffic"
+file_name <- "Mobility_Signaling_Peering_Traffic"
+suffix <- ".csv"
+end_point_in <- paste(base_path, folder, file_name, suffix, sep="")
+end_point_out <- paste(base_path, folder, file_name, "_subsample", suffix, sep="")
+
+cat(file_name, "\n")
+file_in <- file(end_point_in, "r")
+file_out <- file(end_point_out, "w")
+
+# Headers ------------------------------------------------------------------
+headers <- readLines(file_in, n=1)
+cat("Headers:", headers, "\n")
+writeLines(headers, file_out) # copy headers
+
+# Data ------------------------------------------------------------------
+datasample <- readLines(file_in, n=sample_size)
+writeLines(datasample, file_out)
+
+# Wrap up ---------------------------------------------------------------------
+close(file_in)
+close(file_out)
+
+cat('\nSubsample data written.')
